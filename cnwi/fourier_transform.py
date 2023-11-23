1269bce8d0ca80e068f8074ad855769d93882963
Ryan Hamilton <ryan.hamilton@ec.gc.ca>
2023-11-23 10:54:45 -0500

    Add FourierTransform class to compute Fourier transform of time series

--- cnwi/fourier_transform.py
+++ cnwi/fourier_transform.py
@@ -4,7 +4,31 @@
 import ee
 
 from cnwi.cnwilib.image_collection import TimeSeries
-from cnwi.cnwilib.image_math import LinearRegression
+from cnwi.cnwilib.image_math import LinearRegression, Phase, Amplitude
+
+
+class FourierTransform:
+    def __init__(self, time_series: TimeSeries, trend: LinearRegression) -> None:
+        self.time_series = time_series
+        self.trend = trend
+
+    def compute(self) -> ee.Image:
+        """compute the fourier transform of the time series using the trend from the linear regression"""
+        # add coefficients to each image in the collection
+        self.time_series.collection = self.time_series.collection.map(
+            lambda image: image.addBands(self.trend.get_coefficients())
+        )
+
+        # add phase and amplitude to each image in the collection
+        for mode in range(1, self.time_series.modes + 1):
+            phase = Phase(mode)
+            amplitude = Amplitude(mode)
+            self.time_series.collection = self.time_series.collection.map(phase.compute)
+            self.time_series.collection = self.time_series.collection.map(
+                amplitude.compute
+            )
+        selectors = f"{self.time_series.dependent}|.*coef|amp.*|phase.*"
+        return self.time_series.select(selectors).median().unitScale(-1, 1)
 
 
 def compute_fourier_transform(
@@ -22,5 +46,5 @@ def compute_fourier_transform(
     # compute the trend from the time series
     lin_reges = LinearRegression(time_series)
     # compute the Fourier Transform of the residuals
-    fourier_transform = time_series.fourier_transform(lin_reges)
+    fourier_transform = FourierTransform(time_series, lin_reges).compute()
     return fourier_transform
