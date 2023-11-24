02e3b7c86f58100f24746d3ff9fcea85e5309134
Ryan Hamilton <ryan.hamilton@ec.gc.ca>
2023-11-24 11:42:14 -0500

    Refactor feature insertion and add data lookup functionality

--- cnwi/cnwilib/features.py
+++ cnwi/cnwilib/features.py
@@ -9,31 +9,29 @@ import pandas as pd
 
 
 def insert_values_into_features(
-    features: gpd.GeoDataFrame, values: pd.Series
+    features: gpd.GeoDataFrame, lookup: pd.DataFrame, column: str = None
 ) -> gpd.GeoDataFrame:
-    pass
+    column = column or "class_name"
+    return features.merge(lookup, on=column, how="inner")
 
 
-def create_data_lookup(labels: list[str] | pd.Series) -> pd.DataFrame:
-    pass
-
-
-def add_region_to_features():
-    pass
+def create_data_lookup(labels: pd.Series) -> pd.DataFrame:
+    if not isinstance(labels, pd.Series):
+        raise TypeError("labels must be a pandas Series")
+    unique_labels = labels.unique().tolist()
+    return pd.DataFrame(
+        {"class_name": unique_labels, "value": list(range(1, len(unique_labels) + 1))}
+    )
 
 
 def insert_is_training(
     training_features: gpd.GeoDataFrame, testing_features: gpd.GeoDataFrame
-) -> gpd.GeoDataFrame:
-    pass
-
-
-def load_from_data_manifest(manifest: pd.DataFrame) -> gpd.GeoDataFrame:
-    pass
-
-
-def make_data_manifest(file_paths: list[str]) -> pd.DataFrame:
-    pass
+) -> tuple[gpd.GeoDataFrame, pd.DataFrame]:
+    training_features["is_training"] = 1
+    testing_features["is_training"] = 0
+    return pd.concat([training_features, testing_features]), pd.DataFrame(
+        {"is_training": [1, 0], "description": ["training", "testing"]}
+    )
 
 
 ####################################################################################################
@@ -44,4 +42,13 @@ def make_data_manifest(file_paths: list[str]) -> pd.DataFrame:
 def sample_regions(
     image: ee.Image, features: ee.FeatureCollection, **kwargs
 ) -> ee.FeatureCollection:
-    pass
+    """Sample the image at the location of each feature in the feature collection.
+
+    Args:
+        image (ee.Image): The image to sample
+        features (ee.FeatureCollection): The features to sample the image at
+
+    Returns:
+        ee.FeatureCollection: The features with the image values added
+    """
+    return image.sampleRegions(features, **kwargs)
