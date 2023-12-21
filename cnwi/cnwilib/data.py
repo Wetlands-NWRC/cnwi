27ac73bd20d7c07e01ff72338fcebd595ba56f07
Ryan Hamilton <ryan.hamilton@ec.gc.ca>
2023-12-21 15:18:42 -0500

    Refactor data processing functions and add server-side functions

--- cnwi/cnwilib/data.py
+++ cnwi/cnwilib/data.py
@@ -1,11 +1,15 @@
 from __future__ import annotations
+
 import os
 from typing import Iterable, Tuple
 
+import ee
 import geopandas as gpd
 import pandas as pd
 
 
+####################################################################################################
+# Client Side Functions and Classes
 class Manifest:
     """Class representing a manifest of data files."""
 
@@ -117,95 +121,48 @@ class LookupTable:
 
 
 ####################################################################################################
-class ManifestProcessor:
-    def __init__(
-        self, manifest: Manifest, lookup_engine: LookupTable = LookupTable()
-    ) -> None:
-        self.manifest = manifest
-        self.lookup_engine = lookup_engine
-
-    @staticmethod
-    def _process_shapefile(row: pd.Series, **kwargs) -> gpd.GeoDataFrame:
-        gdf = gpd.read_file(row["file_path"], kwargs=kwargs)
-        if gdf.crs != "EPSG:4326":
-            gdf = gdf.to_crs("EPSG:4326")
-        gdf["type"] = row["type"]
-        gdf["region_id"] = row["region_id"]
-        return gdf
-
-    def process(self) -> ManifestProcessor:
-        gdfs = []
-        for _, group in self.manifest:
-            for _, row in group[group["type"] != 3].iterrows():
-                gdf = self._process_shp(row)
-                gdfs.append(gdf)
-        self.processed_manifest = gpd.GeoDataFrame(pd.concat(gdfs))
-        return self
-
-    def save(self, where: str, driver: str = None) -> ManifestProcessor:
-        driver = driver or "ESRI Shapefile"
-        self.processed_manifest.to_file(where, driver="ESRI Shapefile")
-        return self
-
-
-####################################################################################################
-
-
-def get_shapefile_paths(data_dir: str) -> list[str]:
-    """Returns a list of paths to all shapefiles in the data directory."""
-    shapefile_paths = []
-    for root, dirs, files in os.walk(data_dir):
-        for file in files:
-            if file.endswith(".shp"):
-                shapefile_paths.append(os.path.join(root, file))
-    return shapefile_paths
-
-
-def create_raw_data_manifest(file_paths: list[str]) -> pd.DataFrame:
-    """Returns a dataframe with the file path, region id, and type of data."""
-    df = pd.DataFrame(file_paths, columns=["file_path"])
-    # lookup -> map int value to represent type 1, 2, 3 (training, validation, region)
-    # region_id
-    df["region_id"] = df["file_path"].str.extract(r"(\b\d{1,3}\b)")
-    # type assign a int value to each type
-    df["type"] = df["file_path"].str.extract(r"/(\w+)(?:Points)?\.shp")
-    # training = 0, validation = 1, region = 2
-    df["type"] = df["type"].map(
-        {"trainingPoints": 1, "validationPoints": 2, "region": 3}
-    )
-    return df
-
-
-def process_data_manifest(df: pd.DataFrame) -> gpd.GeoDataFrame:
-    """Returns a GeoDataFrame with the class name, geometry, type, and region id."""
-    processed_gdfs = []
-
-    # ony need to process the training and validation data
-    for _, row in df.iterrows():
-        if row["type"] == 1 or row["type"] == 2:
-            processed_gdfs.append(process_shapefile(row, driver="ESRI Shapefile"))
-    return gpd.GeoDataFrame(pd.concat(processed_gdfs))
-
-
 def process_shapefile(row: pd.Series, **kwargs) -> gpd.GeoDataFrame:
     gdf = gpd.read_file(row["file_path"], kwargs=kwargs)
     if gdf.crs != "EPSG:4326":
         gdf = gdf.to_crs("EPSG:4326")
     gdf["type"] = row["type"]
-    gdf["x"] = gdf.geometry.x
-    gdf["y"] = gdf.geometry.y
     gdf["region_id"] = row["region_id"]
     return gdf
 
 
-def create_lookup_table(df: gpd.GeoDataFrame, col: str = None) -> pd.DataFrame:
-    col = col or "class_name"
-    unique_labels = df[col].unique().tolist()
-    return pd.DataFrame(
-        {"class_name": unique_labels, "value": list(range(1, len(unique_labels) + 1))}
-    )
+def process_data_manifest(
+    manifest: Manifest, label_col: str
+) -> Tuple[gpd.GeoDataFrame, LookupTable]:
+    """Returns a GeoDataFrame with the class name, geometry, type, and region id."""
+    processed_gdfs = []
+
+    # ony need to process the training and validation data
+    for _, group in manifest:
+        for _, row in group[group["type"] != 3].iterrows():
+            gdf = process_shapefile(row, driver="ESRI Shapefile")
+            processed_gdfs.append(gdf)
+    combined = gpd.GeoDataFrame(pd.concat(processed_gdfs))
+    lookup_table = LookupTable(combined[label_col].unique().tolist())
+    combined[label_col] = combined[label_col].map(lookup_table.mapper)
+    return combined, lookup_table
 
 
-def create_processed_data_manifest(file_paths: list[str]) -> pd.DataFrame:
-    # create a manifest of the processed data and regions
-    pass
+####################################################################################################
+# Server Side Functions
+
+
+def sample_regions(image: ee.Image, **kwargs) -> ee.FeatureCollection:
+    """
+    Sample regions from an image using the specified parameters.
+
+    Args:
+        image (ee.Image): The image to sample regions from.
+        **kwargs: Additional keyword arguments to be passed to the `sampleRegions` method.
+
+    Returns:
+        ee.FeatureCollection: A feature collection containing the sampled regions.
+    """
+    return image.sampleRegions(**kwargs)
+
+
+####################################################################################################
