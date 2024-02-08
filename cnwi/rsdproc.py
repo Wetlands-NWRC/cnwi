4342539c18a734fa31039cf21c16b73027e44efe
Ryan Hamilton <ryan.hamilton@ec.gc.ca>
2024-02-08 15:52:55 -0500

    update

--- cnwi/rsdproc.py
+++ cnwi/rsdproc.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+from math import pi
+from typing import Callable, Any
 
 # Remote Sensing Data Processing
 # cnwi/rsdproc.py
@@ -11,26 +13,64 @@ from ee.geometry import Geometry
 from ee.imagecollection import ImageCollection
 
 
-# Calculators
-def add_ndvi(nir: str, red: str):
+# SAR Processing and Datasets
+
+
+###############################################################################################
+
+
+def s1_dataset(constructor: Any, aoi) -> tuple[ee.Image, ee.Image]:
+    dataset = (
+        ee.ImageCollection(constructor)
+        .filterBounds(aoi)
+        .select("V.*")
+        .map(lambda x: x.convolve(ee.Kernel.square(1)))
+        .map(
+            lambda x: x.addBands(x.select("VV").divide(x.select("VH")).rename("VV_VH"))
+        )
+    )
+    es = dataset.filterDate("2017-01-01", "2017-12-31").mosaic()
+    ls = dataset.filterDate("2018-01-01", "2018-12-31").mosaic()
+    return es, ls
+
+
+def alos_dataset(aoi) -> ee.Image:
+    start, end = "2018", "2021"
+    dataset = (
+        ee.ImageCollection("JAXA/ALOS/PALSAR/YEARLY/SAR")
+        .filterBounds(aoi)
+        .filterDate(start, end)
+        .map(lambda x: x.convolve(ee.Kernel.square(1)))
+        .map(
+            lambda x: x.addBands(x.select("HH").divide(x.select("HV")).rename("HH_HV"))
+        )
+        .median()
+    )
+    return dataset
+
+
+###############################################################################################
+
+
+def compute_ndvi(nir: str, red: str) -> Callable[[ee.Image], ee.Image]:
     """
-    Adds the NDVI band to an image.
+    Computes the NDVI from the NIR and red bands.
 
     Args:
         nir (str): The name of the NIR band.
         red (str): The name of the red band.
 
     Returns:
-        ee.Image: The image with the NDVI band added.
+        Callable[[ee.Image], ee.Image]: A function that computes the NDVI.
     """
     return lambda image: image.addBands(
         image.normalizedDifference([nir, red]).rename("NDVI")
     )
 
 
-def add_savi(nir: str, red: str, l: float = 0.5):
+def compute_savi(nir: str, red: str, l: float = 0.5) -> Callable[[ee.Image], ee.Image]:
     """
-    Adds the SAVI band to an image.
+    Computes the SAVI from the NIR and red bands.
 
     Args:
         nir (str): The name of the NIR band.
@@ -38,7 +78,7 @@ def add_savi(nir: str, red: str, l: float = 0.5):
         l (float, optional): The SAVI coefficient. Defaults to 0.5.
 
     Returns:
-        ee.Image: The image with the SAVI band added.
+        Callable[[ee.Image], ee.Image]: A function that computes the SAVI.
     """
     return lambda image: image.addBands(
         image.expression(
@@ -48,7 +88,9 @@ def add_savi(nir: str, red: str, l: float = 0.5):
     )
 
 
-def add_tasseled_cap(blue: str, green: str, red: str, nir: str, swir1: str, swir2: str):
+def compute_tasseled_cap(
+    blue: str, green: str, red: str, nir: str, swir1: str, swir2: str
+):
     """
     Adds the Tasseled Cap bands to an image.
 
@@ -94,282 +136,122 @@ def add_tasseled_cap(blue: str, green: str, red: str, nir: str, swir1: str, swir
     return compute
 
 
-def ratio(b1, b2, name):
-    """
-    Adds the ratio band to an image.
+def data_cube_dataset(dataset_id, aoi) -> ee.Image:
 
-    Args:
-        b1 (str): The name of the first band.
-        b2 (str): The name of the second band.
-        name (str): The name of the ratio band.
+    spring_bands = ["B2", "B3", "B4", "B5", "B6", "B7", "B8", "B8A", "B11", "B12"]
+    summer_bands = [f"{band}_1" for band in spring_bands]
+    fall_bands = [f"{band}_2" for band in spring_bands]
 
-    Returns:
-        ee.Image: The image with the ratio band added.
-    """
-    return lambda image: image.addBands(
-        image.expression(
-            "((NIR - RED) / (NIR + RED + 0.5)) * (1.5)",
-            {"NIR": image.select(b1), "RED": image.select(b2)},
-        ).rename(name)
+    bands = spring_bands + summer_bands + fall_bands
+    b_pattern = "a_spri_b0[2-9].*|a_spri_b[1-2].*|b_summ_b0[2-9].*|b_summ_b[1-2].*|c_fall_b0[2-9].*|c_fall_b[1-2].*"
+
+    dataset = ee.ImageCollection(dataset_id).filterBounds(aoi).select(b_pattern)
+
+    original_bands = dataset.first().bandNames()
+    dataset = (
+        dataset.select(original_bands, bands)
+        .map(compute_ndvi("B8", "B4"))
+        .map(compute_ndvi("B8_1", "B4_1"))
+        .map(compute_ndvi("B8_2", "B4_2"))
+        .map(compute_savi("B8", "B4"))
+        .map(compute_savi("B8_1", "B4_1"))
+        .map(compute_savi("B8_2", "B4_2"))
+        .map(compute_tasseled_cap("B2", "B3", "B4", "B8", "B11", "B12"))
+        .map(compute_tasseled_cap("B2_1", "B3_1", "B4_1", "B8_1", "B11_1", "B12_1"))
+        .map(compute_tasseled_cap("B2_2", "B3_2", "B4_2", "B8_2", "B11_2", "B12_2"))
+    )
+    return dataset.mosaic()
+
+
+def compute_s2_time_series(aoi, modes: int = 3) -> ImageCollection:
+    mk_freq = lambda x, y: [f"{x}_{y}" for y in range(1, modes + 1)]
+
+    def cloud_mask(image: ee.Image):
+        qa = image.select("QA60")
+
+        # Bits 10 and 11 are clouds and cirrus, respectively.
+        cloud_bit_mask = 1 << 10
+        cirrus_bit_mask = 1 << 11
+
+        # Both flags should be set to zero, indicating clear conditions.
+        mask = (
+            qa.bitwiseAnd(cloud_bit_mask)
+            .eq(0)
+            .And(qa.bitwiseAnd(cirrus_bit_mask).eq(0))
+        )
+
+        return image.updateMask(mask)
+
+    def add_time(image: ee.Image):
+        date = image.date()
+        years = date.difference(ee.Date("1970-01-01"), "year")
+        time_radians = ee.Image(years.multiply(2 * pi))
+        return image.addBands(time_radians.rename("t").float())
+
+    def add_harmonics(frequencies: list[int], cos_names, sin_names):
+        def wrapper(image: ee.Image):
+            frequencies = ee.Image.constant(frequencies)
+            time = ee.Image(image).select("t")
+            cos_img = time.multiply(frequencies).cos().rename(cos_names)
+            sin_img = time.multiply(frequencies).sin().rename(sin_names)
+            return image.addBands(cos_img).addBands(sin_img)
+
+        return wrapper
+
+    def compute_phase(mode: int, cos_name: str, sin_name: str):
+        def wrapper(image: ee.Image):
+            phase = ee.Image(
+                image.select(cos_name)
+                .atan2(image.select(sin_name))
+                .rename(f"phase_{mode}")
+            )
+            return image.addBands(phase)
+
+        return wrapper
+
+    def compute_amplitude(mode: int, cos_name: str, sin_name: str):
+        def wrapper(image: ee.Image):
+            amplitude = ee.Image(
+                image.select(cos_name)
+                .hypot(image.select(sin_name))
+                .rename(f"amplitude_{mode}")
+            )
+            return image.addBands(amplitude)
+
+        return wrapper
+
+    ## TS Processing starts here
+
+    cos_name = mk_freq("cos", modes)
+    sin_name = mk_freq("sin", modes)
+
+    dependent = "NDVI"
+    independent = ["t", "constant"] + cos_name + sin_name
+
+    dataset = (
+        ee.ImageCollection("COPERNICUS/S2_HARMONIZED")
+        .filterBounds(aoi)
+        .filterDate("2017-01-01", "2022-12-31")
+        .map(cloud_mask)
+        .map(compute_ndvi("B8", "B4"))
+        .select("NDVI")
+        .map(lambda x: x.addBands(ee.Image(1)))
+        .map(add_time)
+        .map(add_harmonics(list(range(1, modes + 1)), cos_name, sin_name))
     )
 
+    harmonic_trend = dataset.select(independent + [dependent]).reduce(
+        ee.Reducer.linearRegression(len(independent), 1)
+    )
 
-###############################################################################################
-# Filters for Sentinel 1
-def boxcar(image: ee.Image) -> ee.Image:
-    """
-    Applies a boxcar filter to the input image.
+    harmonic_coeff = harmonic_trend.select("coefficients").arrayFlatten(
+        [independent, ["coef"]]
+    )
 
-    Args:
-        image (ee.Image): The input image to be filtered.
+    dataset = dataset.map(lambda x: x.addBands(harmonic_coeff))
 
-    Returns:
-        ee.Image: The filtered image.
-    """
-    return image.convolve(ee.Kernel.square(1))
+    for mode in range(1, modes + 1):
+        dataset = dataset.map(compute_phase(mode, f"cos_{mode}", f"sin_{mode}"))
+        dataset = dataset.map(compute_amplitude(mode, f"cos_{mode}", f"sin_{mode}"))
 
-
-###############################################################################################
-
-
-class RSDProc:
-    """Remote Sensing Data Processing"""
-
-    def __init__(
-        self,
-        collection: ee.ImageCollection,
-        region: ee.Geometry | ee.FeatureCollection,
-        start_date: str = None,
-        end_date: str = None,
-    ) -> None:
-        self.collection = collection
-        self.region = region
-        self.start_date = start_date
-        self.end_date = end_date
-
-    def __add__(self, other: RSDProc) -> RSDProc:
-        self.collection = self.collection.merge(other.collection)
-        return self
-
-    def filter_geometry(self) -> RSDProc:
-        self.collection = self.collection.filterBounds(self.region)
-        return self
-
-    def filter_date(self) -> RSDProc:
-        if self.start_date or self.end_date:
-            return self
-        self.collection = self.collection.filterDate(self.start_date, self.end_date)
-        return self
-
-    def select_bands(self, pattern: str) -> RSDProc:
-        self.collection = self.collection.select(pattern)
-        return self
-
-    def rename_bands(self, selectors: list[str], names: list[str]) -> RSDProc:
-        self.collection = self.collection.select(selectors, names)
-        return self
-
-    def add_func(self, func: callable) -> RSDProc:
-        self.collection = self.collection.map(func)
-        return self
-
-    def process(self) -> RSDProc:
-        return self
-
-
-class DataCubeProc(RSDProc):
-    """Data Cube Processing"""
-
-    BAND_NAMES = ["B2", "B3", "B4", "B5", "B6", "B7", "B8", "B8A", "B11", "B12"]
-
-    def __init__(self, collection: ee.ImageCollection, region) -> None:
-        super().__init__(collection, region, None, None)
-
-    def select_spectral_bands(self) -> DataCubeProc:
-        p = "a_spri_b0[2-9].*|a_spri_b[1-2].*|b_summ_b0[2-9].*|b_summ_b[1-2].*|c_fall_b0[2-9].*|c_fall_b[1-2].*"
-        self.select_bands(p)
-        return self
-
-    def rename_spectral_bands(self) -> DataCubeProc:
-        bands = self._mk_band_names(self.BAND_NAMES)
-        self.rename_bands(self.collection.first().bandNames(), bands)
-        return self
-
-    def add_spring_ndvi(self) -> DataCubeProc:
-        self.add_func(add_ndvi("B8", "B4"))
-        return self
-
-    def add_spring_savi(self) -> DataCubeProc:
-        self.add_func(add_savi("B8", "B4"))
-        return self
-
-    def add_spring_tasseled_cap(self) -> DataCubeProc:
-        self.add_func(add_tasseled_cap("B2", "B3", "B4", "B8", "B11", "B12"))
-        return self
-
-    def add_summer_ndvi(self) -> DataCubeProc:
-        self.add_func(add_ndvi("B8_1", "B4_1"))
-        return self
-
-    def add_summer_savi(self) -> DataCubeProc:
-        self.add_func(add_savi("B8_1", "B4_1"))
-        return self
-
-    def add_summer_tasseled_cap(self) -> DataCubeProc:
-        self.add_func(
-            add_tasseled_cap("B2_1", "B3_1", "B4_1", "B8_1", "B11_1", "B12_1")
-        )
-        return self
-
-    def add_fall_ndvi(self) -> DataCubeProc:
-        self.add_func(add_ndvi("B8_2", "B4_2"))
-        return self
-
-    def add_fall_savi(self) -> DataCubeProc:
-        self.add_func(add_savi("B8_2", "B4_2"))
-        return self
-
-    def add_fall_tasseled_cap(self) -> DataCubeProc:
-        self.add_func(
-            add_tasseled_cap("B2_2", "B3_2", "B4_2", "B8_2", "B11_2", "B12_2")
-        )
-        return self
-
-    def process(self) -> DataCubeProc:
-        (
-            self.filter_geometry()
-            .select_spectral_bands()
-            .rename_spectral_bands()
-            .add_spring_ndvi()
-            .add_spring_savi()
-            .add_spring_tasseled_cap()
-            .add_summer_ndvi()
-            .add_summer_savi()
-            .add_summer_tasseled_cap()
-            .add_fall_ndvi()
-            .add_fall_savi()
-            .add_fall_tasseled_cap()
-        )
-        return self
-
-    @staticmethod
-    def _mk_band_names(bands: list[str]) -> list[str]:
-        summer = [f"{band}_1" for band in bands]
-        fall = [f"{band}_2" for band in bands]
-        return bands + summer + fall
-
-
-class S1Proc(RSDProc):
-    """Sentinel 1 Processing"""
-
-    def __init__(
-        self,
-        collection: ee.ImageCollection,
-        region: ee.Geometry | ee.FeatureCollection,
-        start_date: str = None,
-        end_date: str = None,
-    ) -> None:
-        super().__init__(collection, region, start_date, end_date)
-
-    def denoise(self) -> S1Proc:
-        self.add_func(boxcar)
-        return self
-
-    def add_ratio(self):
-        self.add_func(ratio("VV", "VH", "VV_VH"))
-        return self
-
-    def process(self) -> S1Proc:
-        (self.filter_geometry().filter_date().select_bands("V.*").denoise().add_ratio())
-        return self
-
-
-class ALOSProc(RSDProc):
-    """ALOS Processing"""
-
-    def __init__(
-        self,
-        region: ee.Geometry | ee.FeatureCollection,
-        start_date: str,
-        end_date: str,
-    ) -> None:
-        super().__init__(
-            ee.ImageCollection("JAXA/ALOS/PALSAR/YEARLY/SAR_EPOCH"),
-            region,
-            start_date,
-            end_date,
-        )
-
-    def denoise(self) -> ALOSProc:
-        self.add_func(boxcar)
-        return self
-
-    def add_ratio(self) -> ALOSProc:
-        self.add_func(ratio("HH", "HV", "HH_HV"))
-        return self
-
-    def process(self) -> ALOSProc:
-        (self.filter_geometry().filter_date().select_bands("H.*").denoise().add_ratio())
-        return self
-
-
-class S2Proc(RSDProc):
-    def __init__(
-        self,
-        region: Geometry | FeatureCollection,
-        start_date: str,
-        end_date: str,
-    ) -> None:
-        super().__init__(
-            ee.ImageCollection("COPERNICUS/S2_HARMONIZED"), region, start_date, end_date
-        )
-
-    def filter_cloud_cover(self, max_cloud_cover: float = 20) -> S2Proc:
-        self.collection = self.collection.filter(
-            ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE", max_cloud_cover)
-        )
-        return self
-
-    def add_cloud_mask(self) -> S2Proc:
-        def _mask(image: ee.Image):
-            qa = image.select("QA60")
-
-            # Bits 10 and 11 are clouds and cirrus, respectively.
-            cloud_bit_mask = 1 << 10
-            cirrus_bit_mask = 1 << 11
-
-            # Both flags should be set to zero, indicating clear conditions.
-            mask = (
-                qa.bitwiseAnd(cloud_bit_mask)
-                .eq(0)
-                .And(qa.bitwiseAnd(cirrus_bit_mask).eq(0))
-            )
-
-            return image.updateMask(mask)
-
-        self.add_func(_mask)
-        return self
-
-    def add_ndvi(self) -> S2Proc:
-        self.add_func(add_ndvi("B8", "B4"))
-        return self
-
-    def add_savi(self) -> S2Proc:
-        self.add_func(add_savi("B8", "B4"))
-        return self
-
-    def add_tasseled_cap(self) -> S2Proc:
-        self.add_func(add_tasseled_cap("B2", "B3", "B4", "B8", "B11", "B12"))
-        return self
-
-    def process(self) -> S2Proc:
-        (
-            self.filter_geometry()
-            .filter_date()
-            .filter_cloud_cover()
-            .add_cloud_mask()
-            .add_ndvi()
-            .add_savi()
-            .add_tasseled_cap()
-        )
-        return self
+    return dataset.median().select(f"{dependent}|.*coef|amp.*|phase.*").unitScale(-1, 1)
